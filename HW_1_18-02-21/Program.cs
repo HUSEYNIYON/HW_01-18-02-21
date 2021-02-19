d0564994c97a814d4764590aa498615a000fd66f
Huseyn <huseyn19.burhonov1999@gmail.com>
2021-02-19 19:37:56 +0500

    Method threadSum Summa SumAsync

--- HW_1_18-02-21/Program.cs
+++ HW_1_18-02-21/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace HW_1_18_02_21
 {
@@ -6,7 +8,33 @@ namespace HW_1_18_02_21
     {
         static void Main(string[] args)
         {
-            
+            Console.SetWindowSize(170, 42);
+            for (int i = 0; i < 160; i++)
+            {
+                Matrix matrix = new Matrix(i, true);
+                new Thread(matrix.Running).Start();
+            }
         }
+        public static void threadSum()
+        {
+            Thread thread = new Thread(new ThreadStart(threadSum));
+            thread.Start();
+        }
+        public static void Summa(int begin, int end)
+        {
+            int result = 0;
+            for (int i = begin; i < end; i++)
+            {
+                result += i;
+            }
+        }
+        static async void SumAsync()
+        {
+            await Task.Run(() => Summa(1,5));
+            await Task.Run(() => Summa(5, 10));
+            await Task.Run(() => Summa(2, 5));
+            await Task.Run(() => Summa(10, 18));
+        }
+
     }
 }
